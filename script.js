30ad68f3b7d818a4a134712ffdea85e9cd0a1b11
breaun1 <58483508+breaun1@users.noreply.github.com>
2024-01-07 02:02:19 +0000

    e

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("Hello World")
+
+const HelloWorld = console.log("Hello World")
+console.log(HelloWorld)
+
+function Hello_World(helloWorld){
+
+
+  console.log("This file logs Hello World three different ways")
+  console.log(`You can say ${helloWorld}, instead of the usual Hello World`);
+  }
