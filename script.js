fde158a67e7729c29c0bf1ea5e561f8fb9a56560
breaun1 <58483508+breaun1@users.noreply.github.com>
2024-01-07 02:19:56 +0000

    Wrote out a few logs of hello_World

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-console.log("Hello World")
 
-const HelloWorld = console.log("Hello World")
+console.log("Hello World") //Simple console log
+
+const HelloWorld = console.log("Hello World") //Store the log in a variable
 console.log(HelloWorld)
 
-function Hello_World(helloWorld){
+function Hello_World(helloWorld){ //Stored the log in a function with a dynamic way to use it
 
 
   console.log("This file logs Hello World three different ways")
-  console.log(`You can say ${helloWorld}, instead of the usual Hello World`);
+  console.log(`You can say ${helloWorld}, instead of the dreaded "Hello World"`);
   }
+
+  Hello_World("Good Afternoon")
